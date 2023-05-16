9a32e13e77c9ad6addcf125e0b7f61c5191407bd
Didier <didier002010@gmail.com>
2023-05-16 01:47:25 -0500

    segundo commit

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-alert("hola mundo")
+//alert("hola mundo");   //hola mundo
+
+// comentarios de linea 
+/* comentarios de mas de una linea 
+*/
+
+// sentencias 
+//alert('hola'); alert('mundo');
+//alert('hola');
+//alert('mundo');
+
+//variables
+let a=5;
+let b=6;
+let suma= a+b;
+console.log("el resultado de la suma es "+ suma)
+
+let nombre= "roberto"
+let nombre_dos= nombre 
+
+nombre_dos= "carlos"
+console.log("el primer nombre es "+nombre)
+console.log("el segundo nombre es "+nombre_dos)
+
+// notaciones de variables
+
+camelcase =5
+snake_case= 5
+pascal_case= 5
+
+let nombre_completo= "didier silva amador "
+
+// no puedes iniciar con numero 
+//no puede tener guion medio
+
+
+//constantes 
+const PI =3.1416;
+const COLOR="rojo";
